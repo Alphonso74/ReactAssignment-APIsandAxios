c3383919d86c90b2a63cb73bef7b7bf7f0c3be94
Alphonso74 <ajm6684@psu.edu>
2020-10-23 16:58:46 -0400

    Request and Post to API endpoint

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -7,24 +7,53 @@ import './Blog.css';
 
 class Blog extends Component {
 
+    state = {
+
+        posts: [],
+        selectedPostId: null
+    };
+
     componentDidMount() {
         axios.get('https://jsonplaceholder.typicode.com/posts')
             .then(response => {
-
-                console.log(response);
+                const posts = response.data.slice(0,4);
+                const updatedPosts = posts.map(post => {
+                    return {
+                        ...post,
+                        author: 'Max'
+                    }
+                });
+                this.setState({posts: updatedPosts});
+                // console.log(response);
             })
     }
 
+    postSelectedHandler = (id) => {
+
+        this.setState({selectedPostId: id})
+        // console.log(id);
+    };
+
     render () {
+
+        const posts = this.state.posts.map((post => {
+            return <Post
+                clicked={() =>  this.postSelectedHandler(post.id)}
+                key={post.id}
+                title={post.title}
+                         author={post.author}/>
+            })
+
+
+        );
+
         return (
             <div>
                 <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
+                    {posts}
                 </section>
                 <section>
-                    <FullPost />
+                    <FullPost id={this.state.selectedPostId} />
                 </section>
                 <section>
                     <NewPost />
